b2df9f0c7c3a8e6e78273423f47fef4b8412d14f
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 19:00:49 +0530

    Data Types->Done

---
+++ Basics1/DataTypes.java
@@ -0,0 +1,13 @@
+package Basics1;
+public class DataTypes { 
+   public static void main(String[] args) {
+    byte num1 = 1;
+    long num2 = -40987654;
+    short num3 = 32277;
+    float num4 = 12233255543527.32254534552334534534533332f;
+    System.out.println(num1); 
+    System.out.println(num2); 
+    System.out.println(num3); 
+    System.out.println(num4);  
+   }
+}
