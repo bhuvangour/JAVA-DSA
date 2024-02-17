e6c82962f8c19e500c193b77c50b6f25c4e94462
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 19:05:01 +0530

    Booleans->Done

---
+++ Basics1/Booleans.java
@@ -0,0 +1,9 @@
+package Basics1;
+
+public class Booleans { 
+   public static void main(String[] args) {
+    boolean MyNameIsBhuvan = true;
+    boolean MyNameIsVed = false;
+    System.out.println(MyNameIsBhuvan); 
+   }
+}
