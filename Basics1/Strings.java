6d179251c248f41a8e2c9719bc7676a296d2a212
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 22:29:42 +0530

    Strings->Done

---
+++ Basics1/Strings.java
@@ -0,0 +1,11 @@
+package Basics1;
+public class Strings { 
+   public static void main(String[] args) {
+    String[] fruits = {"apple","banana","mango","cherry"};   
+    System.out.println(fruits[3]);//Access the Elements of an Array
+    String[] cars = {"porsche","Bmw","Mercedes","ferrari"};
+    cars[1]="Mclaren";//Change an Array Element
+    System.out.println(cars[1]);//Array Length
+    System.out.println(cars.length);
+   }
+}
