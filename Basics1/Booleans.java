a6b8374dd429a2c305471174b23154d1349dbd0b
bhuvangour <bhuvangour990@gmail.com>
2024-02-18 01:08:42 +0530

    IfElse->Done

--- Basics1/Booleans.java
+++ Basics1/Booleans.java
@@ -5,5 +5,8 @@ public class Booleans {
     boolean MyNameIsBhuvan = true;
     boolean MyNameIsVed = false;
     System.out.println(MyNameIsBhuvan); 
+    int x = 10;
+    System.out.println(x == 10); // returns true, because the value of x is equal to 10
+    System.out.println(10 == 15); // returns false, because 10 is not equal to 15
    }
 }
---
+++ Basics1/IfElse.java
@@ -0,0 +1,12 @@
+package Basics1;
+
+public class IfElse { 
+   public static void main(String[] args) {
+    int i = 29;
+    if (i%2==0) {
+        System.out.println("Its Even");
+    } else {
+        System.out.println("Its Odd"); 
+     }    
+   }
+}
