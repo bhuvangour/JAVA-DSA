96d3cd8cc9bc5521c5b9ad813d97da38fa0b0caf
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 22:52:34 +0530

    Strings->Done

--- Basics1/Strings.java
+++ Basics1/Strings.java
@@ -7,5 +7,9 @@ public class Strings {
     cars[1]="Mclaren";//Change an Array Element
     System.out.println(cars[1]);//Array Length
     System.out.println(cars.length);
+    String[] cars2 = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (int i = 0; i < cars.length; i++) {
+    System.out.println(cars2[i]);
+      }
    }
 }
