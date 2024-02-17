f420bef4feece212a77bd03212a663b673c1c6f8
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 18:45:31 +0530

    Third Program

--- Basics1/MultipleVars.java
+++ Basics1/MultipleVars.java
@@ -1,5 +1,9 @@
 package Basics1;
-
-public class MultipleVars {
-    
+public class MultipleVars { 
+   public static void main(String[] args) {
+    int x = 23;
+    int y = 427;
+    int z = 47253523;
+    System.out.println(x+y+z); 
+   }
 }
