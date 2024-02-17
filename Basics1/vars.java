1ebbd44c37678bdaeae220c200b920e32cf18012
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 18:37:24 +0530

    Second Program

--- Basics1/vars.java
+++ Basics1/vars.java
@@ -1,7 +1,15 @@
 package Basics1;
-
 public class vars { 
    public static void main(String[] args) {
-        System.out.println(); 
+      int num = 1;
+      String Name = "Bhuvan";
+      float num2 = 23.2f;
+      char test = 'B';
+      boolean first = true;
+      System.out.println(num);
+      System.out.println(Name);
+      System.out.println(test); 
+      System.out.println(num2);
+      System.out.println(first);
    }
 }
