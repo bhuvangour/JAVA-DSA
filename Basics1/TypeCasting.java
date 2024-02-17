6fc634628b9a8e5ef23a7eb57e665ee6488e59ec
bhuvangour <bhuvangour990@gmail.com>
2024-02-17 19:15:15 +0530

    Type Casting->Done

--- Basics1/TypeCasting.java
+++ Basics1/TypeCasting.java
@@ -1,8 +1,8 @@
 package Basics1;
 public class TypeCasting { 
    public static void main(String[] args) {
-    int num1 = 32;
-    double num2 = num1;
+    double num1 = 32.32;
+    int num2 = (int)num1;
         System.out.println(num1); 
         System.out.println(num2); 
    }
