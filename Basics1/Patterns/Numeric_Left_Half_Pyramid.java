445a27d008d2762a7a5a523afc5c06f792562cb4
bhuvangour <bhuvangour990@gmail.com>
2024-05-06 22:35:17 +0530

    Patterns->Done

---
+++ Basics1/Patterns/Numeric_Left_Half_Pyramid.java
@@ -0,0 +1,13 @@
+package Basics1.Patterns;
+
+public class Numeric_Left_Half_Pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n ; i++){
+            for(int j=1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
--- Basics1/Patterns/Pyramid.java
+++ Basics1/Patterns/Pyramid.java
@@ -5,12 +5,13 @@ public class Pyramid {
         int n = 5;
             int i;
             for(i=1;i<=n;i++){
-         for(int j=1;j<=n-i;j++){
-System.out.print(" ");
-           }for(int j =1;j<=i;j++){
-System.out.print("*"+" ");
-           }
-           System.out.println();
-    }
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j =1;j<=i;j++){
+                    System.out.print("*"+" ");
+                }
+        System.out.println();
+            }
     }
 }
